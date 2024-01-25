501f5404fb1d3ad05431ef976aa4b8c717430107
skeoswf <125794269+skeoswf@users.noreply.github.com>
2024-01-25 17:40:44 -0600

    Create API directory with teamData.js for API calls and start memberCards.js

---
+++ components/MemberCards.js
@@ -0,0 +1,5 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+// import Link from 'next/link';
